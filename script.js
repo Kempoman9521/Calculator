6153f8714f9566f34df026959f4ca289ccbf4146
Kempoman9521 <alanhazel3@yahoo.com>
2023-06-22 18:53:01 -0400

    create functions that do arithmetic

---
+++ script.js
@@ -0,0 +1,25 @@
+function addNum(num1,num2) {
+    let result = num1 + num2;
+    return result;
+}
+
+
+function subtractNum(num1,num2) {
+    let result = num1 - num2;
+    return result;
+}
+
+function multiplyNum(num1, num2) {
+    let result = num1 * num2;
+    return result;
+}
+
+
+function divideNum(num1,num2) {
+    let result = num1 / num2;
+    return result;
+}
+
+
+
+
